fe89b5de0b4578ff6d96f417f47287aa1af35ac4
Matt Wallace <mbw5861@louisiana.edu>
2015-09-20 04:46:47 -0500

    jesus christ async

--- pointsBot.js
+++ pointsBot.js
@@ -1,72 +1,131 @@
-module.exports = function (req, res, next) 
-{
-  // default roll is 2d6
-  var matches = false;
-  var times = 2;
-  var die = 6;
-  var rolls = [];
-  var total = 0;
-  var botPayload = {};
-  
-  if (req.body.text) 
-  {
-    // parse roll type if specified
-    if(matches)
-	{
-		die = 7;
-    } 
-	else 
-	{
-      // send error message back to user if input is bad
-      return res.status(200).send('<number>d<sides>');
-    }
-  } 
-  
-  
-	botPayload.text = req.body.user_name + ' says hi';
-  
-	botPayload.username = 'pointsbot';
-	
-	botPayload.channel = req.body.channel_id;
-	
-	botPayload.icon_emoji = ':jacob:';
-	
-	function send (payload, callback) 
-	{
-	  var path = process.env.INCOMING_WEBHOOK_PATH;
-	  var uri = 'https://hooks.slack.com/services' + path;
+var SlackBot = require('slackbots');
 
-	  request({
-		uri: uri,
-		method: 'POST',
-		body: JSON.stringify(payload)
-	  }, function (error, response, body) {
-		if (error) {
-		  return callback(error);
+// create a bot 
+var bot = new SlackBot({
+    token: 'xoxb-11024339157-d0D55lrBzmFZDbczLaF08Nqo', // Add a bot https://my.slack.com/services/new/bot and put the token  
+    name: 'PointsBot'
+});
+
+var makeMention = function(userId) {
+    return '<@' + userId + '>';
+};
+
+var isDirect = function(userId, messageText) {
+    var userTag = makeMention(userId);
+    return messageText &&
+           messageText.length >= userTag.length &&
+           messageText.substr(0, userTag.length) === userTag;
+};
+
+var getOnlineHumansForChannel = function(channel) {
+    if (!channel) return [];
+	//return channel.members;
+	
+    return (channel.members || []) .map
+	(
+		function(id) 
+		{ 
+			return slack.users[id]; 
 		}
+	).filter
+	(
+		function(u) 
+		{ 
+			return !!u && !u.is_bot && u.presence === 'active'; 
+		}
+	);
+		
+};
 
-		callback(null, response.statusCode, body);
-		});
-	}
+bot.on('start', function()
+{
+	// more information about additional params https://api.slack.com/methods/chat.postMessage 
+    var params = {
+        icon_emoji: ':cat:'
+    };
+    //bot.postMessageToUser('username', 'meow!', params);
+    //bot.postMessageToChannel('general', 'meow!', params);
 	
-	send(botPayload, function (error, status, body) 
+	//bot.postMessageToGroup('emorangers', 'REACTOR CORE ONLINE', params);
+	
+});
+
+
+
+
+bot.on('open', function () 
+{
+	//console.log(bot.getChannels.length);
+	/*
+    var channels = Object.keys(SlackBot.channels)
+        .map(function (k) { return SlackBot.channels[k]; })
+        .filter(function (c) { return c.is_member; })
+        .map(function (c) { return c.name; });
+
+    var groups = Object.keys(SlackBot.groups)
+        .map(function (k) { return SlackBot.groups[k]; })
+        .filter(function (g) { return g.is_open && !g.is_archived; })
+        .map(function (g) { return g.name; });
+
+    console.log('Welcome to Slack. You are ' + slack.self.name + ' of ' + slack.team.name);
+
+    if (channels.length > 0) {
+        console.log('You are in: ' + channels.join(', '));
+    }
+    else {
+        console.log('You are not in any channels.');
+    }
+
+    if (groups.length > 0) {
+       console.log('As well as: ' + groups.join(', '));
+    }
+	*/
+});
+
+bot.on('message', function(message) 
+{
+	// all ingoing events https://api.slack.com/rtm 
+    console.log(message);
+	
+	var channelList = bot.getChannels();
+	
+	channelList.then( function(data)
 	{
-	  if (error) 
-	  {
-		return next(error);
-	  } 
-	  else if (status !== 200) 
-	  {
-		// inform user that our Incoming WebHook failed
-		return next(new Error('Incoming WebHook: ' + status + ' ' + body));
-	  } 
-	  else 
-	  {
-		return res.status(200).end();
-	  }
+		console.log(data);
 	});
-						  
+	
+	if(message.type === 'message' && isDirect(bot.name, message.text) == true)
+	{
+		
+	}
+	/*
+    var channel = slack.getChannelGroupOrDMByID(message.channel);
+    var user = slack.getUserByID(message.user);
 
-
-
-}
+    if (message.type === 'message' && isDirect(slack.self.id, message.text)) 
+	{
+        var trimmedMessage = message.text.substr(makeMention(slack.self.id).length+1).trim();
+		
+		var messageArray = trimmedMessage.split(" ");
+        
+        var onlineUsers = getOnlineHumansForChannel(channel)
+            .filter(function(u) { return u.id != user.id; })
+            .map(function(u) { return makeMention(u.id); });
+			
+		if(messageArray.length > 3)
+		{
+			bot.postMessageToUser(user, 'nope', params);
+		}
+		
+		
+		else
+		{
+			//channel.send(onlineUsers.join(', ') + '\r\n' + user.real_name + ' said' + trimmedMessage);
+			channel.send(messageArray.join(', '));
+			
+		}
+        
+        
+    }
+	*/
+});
